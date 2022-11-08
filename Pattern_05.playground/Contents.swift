fa24ee6fabc49520e95d44a4cde1f74dc461af1b
Sivaram Yadav <nsr.learnings@gmail.com>
2022-11-08 16:36:40 +0530

    Pattern 05 added

---
+++ Pattern_05.playground/Contents.swift
@@ -0,0 +1,93 @@
+import UIKit
+
+let givenNumber = 5 //9 //10
+
+func printPattern_SpecialChars_05(number: Int) {
+    for row in 1...number {
+        for column in 1...number {
+            if column <= number - row {
+                print(" ", terminator: "")
+            }
+        }
+        for _ in 1...row {
+            print("* ", terminator: "")
+        }
+        print("")
+    }
+}
+
+printPattern_SpecialChars_05(number: givenNumber)
+print("\n")
+
+/*
+ output:
+ 
+     *
+    * *
+   * * *
+  * * * *
+ * * * * *
+ 
+*/
+
+
+// ------------------------------------------------------------
+
+
+func printPattern_Numbers_05(number: Int) {
+    for row in 1...number {
+        for column in 1...number {
+            if column <= number - row {
+                print(" ", terminator: "")
+            }
+        }
+        for column in 1...row {
+            print("\(column) ", terminator: "")
+        }
+        print("")
+    }
+}
+
+printPattern_Numbers_05(number: givenNumber)
+print("\n")
+
+/*
+ output:
+ 
+     1
+    1 2
+   1 2 3
+  1 2 3 4
+ 1 2 3 4 5
+ 
+*/
+
+// ------------------------------------------------------------
+
+func printPattern_Chars_05(number: Int) {
+    for row in 1...number {
+        for column in 1...number {
+            if column <= number - row {
+                print(" ", terminator: "")
+            }
+        }
+        for _ in 1...row {
+            print("A ", terminator: "")
+        }
+        print("")
+    }
+}
+
+printPattern_Chars_05(number: givenNumber)
+print("\n")
+
+/*
+ output:
+ 
+     A
+    A A
+   A A A
+  A A A A
+ A A A A A
+ 
+*/
