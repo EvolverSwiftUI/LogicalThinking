533aba2fdf92623b196373695d41d816612f4a29
Sivaram Yadav <nsr.learnings@gmail.com>
2022-11-08 17:39:24 +0530

    add

--- Pattern_06.playground/Contents.swift
+++ Pattern_06.playground/Contents.swift
@@ -1,3 +1,96 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var givenNumber = 5
+
+func pattrern_specialchars_06(number: Int) {
+    for row in 1...number {
+        for space in 1...number {
+            if space <= number-row {
+                print(" ", terminator: "")
+                print(" ", terminator: "")
+            }
+        }
+        
+        for _ in 1...(2*row-1) {
+            print("*", terminator: "")
+            print(" ", terminator: "")
+        }
+        
+        print("")
+    }
+}
+
+pattrern_specialchars_06(number: givenNumber)
+print("\n")
+
+
+func pattrern_numbers_06(number: Int) {
+    for row in 1...number {
+        for space in 1...number {
+            if space <= number-row {
+                print(" ", terminator: "")
+                print(" ", terminator: "")
+            }
+        }
+        
+        for column in 1...(2*row-1) {
+            print(column, terminator: "")
+            print(" ", terminator: "")
+        }
+        
+        print("")
+    }
+}
+
+pattrern_numbers_06(number: givenNumber)
+print("\n")
+
+
+func pattrern_alphabets_06(number: Int) {
+    for row in 1...number {
+        for space in 1...number {
+            if space <= number-row {
+                print(" ", terminator: "")
+                print(" ", terminator: "")
+            }
+        }
+        
+        for _ in 1...(2*row-1) {
+            print("A", terminator: "")
+            print(" ", terminator: "")
+        }
+        
+        print("")
+    }
+}
+
+pattrern_alphabets_06(number: givenNumber)
+print("\n")
+
+/*
+ 
+ outputs:
+ 
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+
+
+        1
+      1 2 3
+    1 2 3 4 5
+  1 2 3 4 5 6 7
+1 2 3 4 5 6 7 8 9
+
+
+        A
+      A A A
+    A A A A A
+  A A A A A A A
+A A A A A A A A A
+
+ 
+ */
+
